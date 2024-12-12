9fe147a400d89674b8dfa219df8b45fa3e5d349f
Nawinkishore <nawinkishore2004@gmail.com>
2024-12-12 12:29:47 +0530

    feat:sideBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import {Routes,Route} from "react-router-dom"
+import SignUpPage from './pages/auth/signup/signupPage'
+import LoginPage from './pages/auth/login/LoginPage'
+import HomePage from './pages/home/HomePage'
+import Sidebar from './components/common/SideBar'
 const App = () => {
   return (
-    <div className='text-pink-500'>App</div>
+    <div className='flex max-w-6xl mx-auto'>
+      <Sidebar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/login" element={<LoginPage/>}></Route>
+        <Route path="/signup" element={<SignUpPage/>}></Route>
+
+      </Routes>
+    </div>
   )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+import daisyUIThemes from "daisyui/src/theming/themes";
 export default {
   content: [
     "./index.html",
@@ -7,5 +9,20 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    daisyui,
+  ],
+
+  daisyui: {
+    themes: [
+      "light",
+      {
+        black: {
+          ...daisyUIThemes["black"],
+          primary: "rgb(29, 155, 240)",
+          secondary: "rgb(24, 24, 24)",
+        },
+      },
+    ],
+  },
 }
